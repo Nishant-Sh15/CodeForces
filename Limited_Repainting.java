a3468627fd30f56482459fddf8b4b548be3a8def
Nishant Sharma <sh.nishant15j5@gmail.com>
2025-03-14 12:59:48 +0530

    Update Limited_Repainting.java

--- Limited_Repainting.java
+++ Limited_Repainting.java
@@ -2,37 +2,6 @@
 import java.util.*;
 
 public class Limited_Repainting{
-    static int find(ArrayList<Integer> arr,int t,int s,int e){
-        if(s>e){
-            return e;
-        }
-        int m=(s+e)/2;
-        if(m!=arr.size()-1){
-            if(arr.get(m)>t){
-                return find(arr,t,s,m-1);
-            }else{
-                if(arr.get(m+1)>t){
-                    return m;
-                }else{
-                    return find(arr,t,m+1,e);
-                }
-            }
-        }else{
-            if(arr.get(m)>t){
-                return find(arr,t,s,m-1);
-            }else{
-                return m;
-            }
-        }
-    }
-    static void add(ArrayList<Integer> arr,int v){
-        int i=find(arr,v,0,arr.size()-1);
-        if(i==arr.size()-1){
-            arr.add(v);
-        }else{
-            arr.add(i+1,v);
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
